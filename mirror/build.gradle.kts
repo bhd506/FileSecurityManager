9296c5ee94b44783eed353a71907acd0d4f88617
bhd506 <ben.hd.003@proton.me>
2026-09-06 01:28:36 +0100

    Build gradle configured for unit testing

--- mirror/build.gradle.kts
+++ mirror/build.gradle.kts
@@ -9,15 +9,10 @@ repositories {
     mavenCentral()
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
